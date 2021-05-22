7e4435f1b07d3e92c80de35d61c3872bc11a673f
aniket <aniket.umbc@gmail.com>
2021-05-22 08:07:24 +0530

    state useEffect

--- src/App.js
+++ src/App.js
@@ -2,34 +2,30 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 // import LifeCycleDemo from './components/LifeCycleDemo';
 
-const LifeCycleDemo = () => {
-  useEffect(() => {
-    console.log('render');
+function ThreeCounts() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
 
-    return () => console.log('unMount....');
-  }, []);
+  useEffect(() => {
+    console.log('count2 changed!');
+  }, [count2]);
 
   return (
     <div>
-      <h4> Life Cycle Demo</h4>
+      {count1} {count2} {count3}
+      <br />
+      <button onClick={() => setCount1(count1 + 1)}>Increment count1</button>
+      <button onClick={() => setCount2(count2 + 1)}>Increment count2</button>
+      <button onClick={() => setCount3(count3 + 1)}>Increment count3</button>
     </div>
   );
-};
+}
 
 function App() {
-  const [random, setRandom] = useState(Math.random());
-  const [mounted, setMounted] = useState(true);
-  const reRender = () => setRandom(Math.random());
-  const toggle = () => setMounted(!mounted);
   return (
     <>
-      <label className='app'>
-        {random}
-        {mounted}{' '}
-      </label>
-      <button onClick={reRender}>Re-render</button>
-      <button onClick={toggle}>Show/Hide LifecycleDemo</button>
-      {mounted && <LifeCycleDemo />}
+      <ThreeCounts />
     </>
   );
 }
