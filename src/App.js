ad193811ecbd8c0dffd069a49788b1702a4275e1
aniket <aniket.umbc@gmail.com>
2021-05-22 08:14:07 +0530

    useEffect props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-// import LifeCycleDemo from './components/LifeCycleDemo';
-
-function ThreeCounts() {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const [count3, setCount3] = useState(0);
-
-  useEffect(() => {
-    console.log('count2 changed!');
-  }, [count2]);
-
-  return (
-    <div>
-      {count1} {count2} {count3}
-      <br />
-      <button onClick={() => setCount1(count1 + 1)}>Increment count1</button>
-      <button onClick={() => setCount2(count2 + 1)}>Increment count2</button>
-      <button onClick={() => setCount3(count3 + 1)}>Increment count3</button>
-    </div>
-  );
-}
+import LifeCycleDemo from './components/LifeCycleDemo';
 
 function App() {
+  useEffect(() => {
+    console.log('parent state change ', count1);
+  });
+
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
   return (
     <>
-      <ThreeCounts />
+      <div>
+        <LifeCycleDemo a={count1} b={count2} />
+        <button onClick={() => setCount1(count1 + 1)}>Increment count1</button>
+        <button onClick={() => setCount2(count2 + 1)}>Increment count2</button>
+      </div>
     </>
   );
 }
--- src/components/LifeCycleDemo.js
+++ src/components/LifeCycleDemo.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
 
-const LifeCycleDemo = () => {
+const LifeCycleDemo = ({ a, b }) => {
   useEffect(() => {
-    console.log('render');
-
-    return () => console.log('unMount....');
-  }, []);
+    console.log('render useEffect ', a);
+  }, [a]);
 
   return (
     <div>
       <h4> Life Cycle Demo</h4>
+      <div>
+        I've got 2 props: a={a} and b={b}
+      </div>
     </div>
   );
 };
